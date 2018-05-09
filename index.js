2adf8c505aa15d7ec7fb34c91b03bae628a371ad
Prashant Singh Pawar <prashantpawar@gmail.com>
2018-05-08 20:49:22 -0400

    - More progress

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const providers = require('ethers').providers;
 
 //const charSet = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('');
 const charSet = '0123456789ABCDE'.split('');
-const charSetLength = 1; //charSet.length;
-const privateKeyHead = '0x31415926535897932384626433832795028841971693993751058209749'
+const charSetLength = 2; //charSet.length;
+const privateKeyHead = '0xf7fc05423d62add4883b40af6f18522053c51e66717f5b79384bf41c73ce2c31'.substr(0,61);
+//const privateKeyHead = '0xb5e5beb2307e15565b0539706e0c3b43d90fffc9543cd594ddab0aed3b129fd';
+
 
 // Create a wallet object
 let wallet;
@@ -14,10 +16,16 @@ let wallet;
 
 console.log(charSetLength * charSetLength * charSetLength * charSetLength * charSetLength);
 
-const provider = providers.getDefaultProvider()
-console.log(provider);
+//const provider = providers.getDefaultProvider()
+const provider = new providers.JsonRpcProvider('http://localhost:8545');
+/**
+wallet = new Wallet('0xf7fc05423d62add4883b40af6f18522053c51e66717f5b79384bf41c73ce2c31', provider);
+wallet.getBalance().then(bal => {
+  console.log(wallet.address, bal.toString());
+  //process.exit();
+});
+**/
 
-//process.exit();
 
 let privateKeyTail = '';
 let privateKey = '';
@@ -27,12 +35,12 @@ for(var i = 0; i < charSetLength; i++) {
       for(var l = 0; l < charSetLength; l++) {
         for(var m = 0; m < charSetLength; m++) {
           privateKeyTail = charSet[i] + charSet[j] + charSet[k] + charSet[l] + charSet[m];
-          privateKey = privateKeyHead + privateKeyTail
+          privateKey = privateKeyHead + privateKeyTail;
           // Create a wallet object
           wallet = new Wallet(privateKey, provider)
           wallet.getBalance().then(bal => {
-            console.log(wallet.address, bal);
-            process.exit();
+            console.log(privateKey, wallet.address, bal.toString());
+            //process.exit();
           });
         }
       }
